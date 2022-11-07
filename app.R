bcae85d7b131d5e53326d12af743aed513b860c6
RaiderJay <42942879+RaiderJay@users.noreply.github.com>
2022-11-07 15:14:28 -0500

    Update app.R
    updated to support test data

--- app.R
+++ app.R
@@ -14,126 +14,20 @@ library("shinydashboard")
 library("sys")
 library("plotly")
 library("vistime")
+source("./IO.R")
 
 
-df <- read.csv("./data/TestDataCamplan.csv")
-df$start_date <- as.Date(df$start_date, format = "%Y-%m-%d")
-df$end_date <- as.Date(df$end_date, format = "%Y-%m-%d" )
 
-winDF <- df[which(df$LOE == "Win"),]
-peopleDF <- df[which(df$LOE == "People"),]
-innovateDF <- df[which(df$LOE == "Innovate"),]
+print(TestCampdata)
 
-TotalComplete <- sum(na.omit(df$completed))
-TotalIMOs <- length(na.omit(df$completed))
+winDF <- TestCampdata[which(TestCampdata$IMO_LOE == "Win"),]
+peopleDF <- TestCampdata[which(TestCampdata$IMO_LOE == "People"),]
+innovateDF <- TestCampdata[which(TestCampdata$IMO_LOE == "Innovate"),]
 
-
-opApproach = vistime(df, col.event = "IMO_Name", col.group = "LOE", title = "OP Approach", col.start = "start_date", col.end = "end_date")
-PeopleApproach = vistime(peopleDF, col.event = "IMO_Name", col.group = "Sub_LOE", title = "OP Approach: People", col.start = "start_date", col.end = "end_date")
-WinApproach = vistime(winDF, col.event = "IMO_Name", col.group = "Sub_LOE", title = "OP Approach: Win", col.start = "start_date", col.end = "end_date")
-InnovateApproach = vistime(innovateDF, col.event = "IMO_Name", col.group = "Sub_LOE", title = "OP Approach: Innovate", col.start = "start_date", col.end = "end_date")
-
-TotalSpeed <- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = TotalComplete,
-  title = list(text = "Campaign IMO Performance"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 0),
-  gauge = list(
-    axis =list(range = list(NULL, TotalIMOs)),
-    bar = list(color = "darkblue"),
-    steps = list(
-      list(range = c(0, ceiling(TotalIMOs * .25)), color = "red"),
-      list(range = c(ceiling(TotalIMOs * .25), ceiling(TotalIMOs * .75)), color = "orange"),
-      list(range = c(ceiling(TotalIMOs * .75), TotalIMOs), color = "green")
-      ),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = TotalComplete))) 
-
-qrt1Speed<- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = TotalComplete,
-  title = list(text = "Qrt1 IMO Performance"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 0),
-  gauge = list(
-    axis =list(range = list(NULL, TotalIMOs)),
-    bar = list(color = "darkblue"),
-    steps = list(
-      list(range = c(0, ceiling(TotalIMOs * .25)), color = "red"),
-      list(range = c(ceiling(TotalIMOs * .25), ceiling(TotalIMOs * .75)), color = "orange"),
-      list(range = c(ceiling(TotalIMOs * .75), TotalIMOs), color = "green")
-    ),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = TotalComplete)))
-
-qrt2Speed<- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = TotalComplete,
-  title = list(text = "Qrt2 IMO Performance"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 0),
-  gauge = list(
-    axis =list(range = list(NULL, TotalIMOs)),
-    bar = list(color = "darkblue"),
-    steps = list(
-      list(range = c(0, ceiling(TotalIMOs * .25)), color = "red"),
-      list(range = c(ceiling(TotalIMOs * .25), ceiling(TotalIMOs * .75)), color = "orange"),
-      list(range = c(ceiling(TotalIMOs * .75), TotalIMOs), color = "green")
-    ),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = TotalComplete))) 
-
-qrt3Speed<- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = TotalComplete,
-  title = list(text = "Qrt3 IMO Performance"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 0),
-  gauge = list(
-    axis =list(range = list(NULL, TotalIMOs)),
-    bar = list(color = "darkblue"),
-    steps = list(
-      list(range = c(0, ceiling(TotalIMOs * .25)), color = "red"),
-      list(range = c(ceiling(TotalIMOs * .25), ceiling(TotalIMOs * .75)), color = "orange"),
-      list(range = c(ceiling(TotalIMOs * .75), TotalIMOs), color = "green")
-    ),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = TotalComplete))) 
-
-qrt4Speed<- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = TotalComplete,
-  title = list(text = "Qrt4 IMO Performance"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 0),
-  gauge = list(
-    axis =list(range = list(NULL, TotalIMOs)),
-    bar = list(color = "darkblue"),
-    steps = list(
-      list(range = c(0, ceiling(TotalIMOs * .25)), color = "red"),
-      list(range = c(ceiling(TotalIMOs * .25), ceiling(TotalIMOs * .75)), color = "orange"),
-      list(range = c(ceiling(TotalIMOs * .75), TotalIMOs), color = "green")
-    ),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = TotalComplete))) 
-
-#qrt1Speed <- qrt1Speed %>% layout(autosize = F, width = 300, height = 300)
+opApproach = vistime(TestCampdata, col.event = "IMO_Name", col.group = "IMO_LOE", title = "OP Approach", col.start = "IMO_StartDate", col.end = "IMO_ProposedEndDate", col.color = "IMO_Color")
+PeopleApproach = vistime(peopleDF, col.event = "IMO_Name", col.group = "IMO_SubLOE", title = "OP Approach: People", col.start = "IMO_StartDate", col.end = "IMO_ProposedEndDate", col.color = "IMO_Color")
+WinApproach = vistime(winDF, col.event = "IMO_Name", col.group = "IMO_SubLOE", title = "OP Approach: Win", col.start = "IMO_StartDate", col.end = "IMO_ProposedEndDate", col.color = "IMO_Color")
+InnovateApproach = vistime(innovateDF, col.event = "IMO_Name", col.group = "IMO_SubLOE", title = "OP Approach: Innovate", col.start = "IMO_StartDate", col.end = "IMO_ProposedEndDate", col.color = "IMO_Color")
 
 ui <- dashboardPage(
   dashboardHeader(title = "Campaign Dashboard"),
@@ -154,11 +48,11 @@ ui <- dashboardPage(
                                       value= c(Sys.Date(),Sys.Date() + 180),
                                       timeFormat="%Y-%m-%d")
                  ),
-                 tabPanel("Assesments",
+                 tabPanel("Assesments"
                           #tags$img(src = "/images/300.jpeg", alt = "logo"),
-                          TotalSpeed,
-                          box(qrt1Speed), box(qrt2Speed), 
-                          box(qrt3Speed), box(qrt4Speed)
+                          #TotalSpeed,
+                          #box(qrt1Speed), box(qrt2Speed), 
+                          #box(qrt3Speed), box(qrt4Speed)
                           )
                )),
       tabPanel(h4("People LOE"),
