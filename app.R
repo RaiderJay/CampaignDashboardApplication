8cc4494bd6a9dd9863313bf576e068817b7fc8ba
Raymond Johnson <raygjjohnson@gatech.edu>
2022-11-10 18:13:34 -0600

    added changable tabs

--- app.R
+++ app.R
@@ -25,53 +25,104 @@ ui <- dashboardPage(
                            sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
                                              label = "Search..."),
                            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-                           menuItem("Data", tabName = "dashboard", icon = icon("database"))
+                           menuItem("Assesments", tabName = "Assesments", icon = icon("chart-line")),
+                           menuItem("Data", tabName = "data", icon = icon("database"))
                            )
                   )
   ),
   dashboardBody( 
     tags$head( tags$link(rel = "stylesheet", type = "text/css", href = "dashboard.css")),
-    tabsetPanel(
-      tabPanel("Campaign Overview",
-               tabsetPanel(
-                 tabPanel("Operational Approach",
-                          get_OpApproach(TestCampdata, TestHigherData)
-                 ),
-                 tabPanel("Assesments",
-                          get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
-                 )
-               )),
-      tabPanel("People LOE",
-               tabsetPanel(
-                 tabPanel("Op Approach: People",
-                         get_LOE(TestCampdata,"People")
-                 ),
-                 tabPanel("People: Assesments")
-               )),
-      tabPanel("Win LOE",
-               tabsetPanel(
-                 tabPanel("Op Approach: Win",
-                          get_LOE(TestCampdata,"Win")
-                 ),
-                 tabPanel("Win: Assesments")
-               )),
-      tabPanel("Innovate LOE",
-               tabsetPanel(
-                 tabPanel("Op Approach: Innovate",
-                          get_LOE(TestCampdata,"Innovate")
-                 ),
-                 tabPanel("Innovate: Assesments")
-               )),
-      tabPanel("Data",
-               tabsetPanel(
-                 tabPanel("Campaign Data",
-                          rhandsontable(TestCampdata, rowHeaders = NULL) %>% hot_cols("IMO_ID", allowInvalid = TRUE) %>%
-                            hot_cols(colWidths = 100)
-                 ),
-                 tabPanel("Higher HQ Data",
-                 )
-               ))
+    tabItems(
+      tabItem(tabName = "dashboard", 
+              tabsetPanel(
+                tabPanel("Campaign Overview",
+                         tabsetPanel(
+                           tabPanel("Operational Approach",
+                                    get_OpApproach(TestCampdata, TestHigherData)
+                                    ),
+                           tabPanel("Performance Stats",
+                                    get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
+                                    )
+                         )
+                ),
+              tabPanel("People LOE",
+                       tabsetPanel(
+                         tabPanel("Op Approach: People",
+                                  get_LOE(TestCampdata,"People")
+                                  ),
+                         tabPanel("Performance Stats",
+                                  get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
+                                  )
+                         )
+                ),
+              tabPanel("Win LOE",
+                       tabsetPanel(
+                         tabPanel("Op Approach: Win",
+                                  get_LOE(TestCampdata,"Win")
+                         ),
+                         tabPanel("Performance Stats",
+                                  get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
+                         )
+                       )
+              ),
+              tabPanel("Innovate LOE",
+                       tabsetPanel(
+                         tabPanel("Op Approach: Innovate",
+                                  get_LOE(TestCampdata,"Innovate")
+                         ),
+                         tabPanel("Performance Stats",
+                                  get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
+                         )
+                       )
+              )
+
+            )
+      ),
+
+      tabItem(tabName = "data", "test2")
     )
+    
+    #tabsetPanel(  tabName = "dashboard",
+    #  tabPanel("Campaign Overview",
+    #           tabsetPanel(
+    #             tabPanel("Operational Approach",
+    #                      get_OpApproach(TestCampdata, TestHigherData)
+    #             ),
+    #             tabPanel("Assesments",
+    #                      get_completionRate(TestCampdata, Sys.Date(), Sys.Date()+80)
+    #             )
+    #           )),
+    #  tabPanel("People LOE",
+    #           tabsetPanel(
+    #             tabPanel("Op Approach: People",
+    #                     get_LOE(TestCampdata,"People")
+    #             ),
+    #             tabPanel("People: Assesments")
+    #           )),
+    #  tabPanel("Win LOE",
+    #           tabsetPanel(
+    #             tabPanel("Op Approach: Win",
+    #                      get_LOE(TestCampdata,"Win")
+    #             ),
+    #             tabPanel("Win: Assesments")
+    #           )),
+    #  tabPanel("Innovate LOE",
+    #           tabsetPanel(
+    #             tabPanel("Op Approach: Innovate",
+    #                      get_LOE(TestCampdata,"Innovate")
+    #             ),
+    #             tabPanel("Innovate: Assesments")
+    #           )),
+    #  tabPanel("Data",
+    #           tabsetPanel(
+    #             tabPanel("Campaign Data",
+    #                      rhandsontable(TestCampdata, rowHeaders = NULL) %>% hot_cols("IMO_ID", allowInvalid = TRUE) %>%
+    #                        hot_cols(colWidths = 100)
+    #             ),
+    #             tabPanel("Higher HQ Data",
+    #             )
+    #           ))
+    #)
   )
 )
 
